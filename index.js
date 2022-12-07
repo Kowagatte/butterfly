08e4006c78a911376eec1635e29a68146c7f6aea
Nicholas Ryan <nnr635@usask.ca>
2022-12-07 15:13:02 -0600

    moved to mysql2 hopefully fixing crash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const passwords  = require('./passwords.json')
 const express = require('express')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const app = express()
 app.use(express.json())
