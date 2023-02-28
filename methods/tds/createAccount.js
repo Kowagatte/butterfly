61379a6b42e38f4022e032d6782f8f7798985332
Nicholas Ryan <nnr635@usask.ca>
2023-02-28 14:52:35 -0600

    forgot to change mysqlCon to execute args "mysql"

--- methods/tds/createAccount.js
+++ methods/tds/createAccount.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(app, mysql, bcrypt, passwords){
         app.post('/tds/createAccount/', (req, res)=>{
             if(req.body['secret'] == passwords['secret']){
-                mysqlCon.changeUser({database: "tds"}, (err)=>{
+                mysql.changeUser({database: "tds"}, (err)=>{
                     if(err){
                         console.log("Failed to switch DBs")
                         res.status(500)
@@ -14,7 +14,7 @@ module.exports = {
                         const email = mysql.escape(req.body['email'])
                         const username = mysql.escape(req.body['username'])
         
-                        mysqlCon.query(`select * from accounts where username=${username}`, (err, userResult)=>{
+                        mysql.query(`select * from accounts where username=${username}`, (err, userResult)=>{
                             if(userResult.length > 0){
                                 res.status(400)
                                 res.send("Username is already taken.")
@@ -23,7 +23,7 @@ module.exports = {
                                     if(err){
                                         res.status(500)
                                     }else{
-                                        mysqlCon.query(`insert into logins values (${email}, '${hash}')`, (err, result)=>{
+                                        mysql.query(`insert into logins values (${email}, '${hash}')`, (err, result)=>{
                                             if(err){
                                                 res.status(400)
                                                 if(err.code == "ER_DUP_ENTRY"){
@@ -32,7 +32,7 @@ module.exports = {
                                                     res.send(err.code)
                                                 }
                                             }else{
-                                                mysqlCon.query(`insert into accounts values (${username}, ${email}, 1200, now())`, (err, result2)=>{
+                                                mysql.query(`insert into accounts values (${username}, ${email}, 1200, now())`, (err, result2)=>{
                                                     if(result && result2){
                                                         res.status(200)
                                                         res.send("Account Created Succesfully!")
